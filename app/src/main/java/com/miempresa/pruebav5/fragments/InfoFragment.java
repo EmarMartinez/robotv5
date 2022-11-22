0ba2f1dbe19c7d26eafd8773c95b9e206d83ae74
EmarMartinez <emar.martinez@bosonit.com>
2022-11-22 10:54:27 +0100

    3commit 22nov

--- app/src/main/java/com/miempresa/pruebav5/fragments/InfoFragment.java
+++ app/src/main/java/com/miempresa/pruebav5/fragments/InfoFragment.java
@@ -1,8 +1,11 @@
 package com.miempresa.pruebav5.fragments;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,7 +14,9 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.ainirobot.coreservice.client.ApiListener;
+import com.ainirobot.coreservice.client.Definition;
 import com.ainirobot.coreservice.client.RobotApi;
+import com.ainirobot.coreservice.client.robotsetting.RobotSettingApi;
 import com.miempresa.pruebav5.R;
 import com.miempresa.pruebav5.application.ModuleCallback;
 
@@ -25,7 +30,6 @@ public class InfoFragment extends Fragment {
     TextView labelInfo;
     TextView tvInfo;
     Context context;
-
 
 
     public InfoFragment() {
@@ -42,16 +46,24 @@ public class InfoFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+
         return inflater.inflate(R.layout.fragment_info, container, false);
     }
 
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tvBattery = (TextView) view.findViewById(R.id.battery);
+        tvBattery.setText("La carga actual es: " + RobotSettingApi.getInstance().getRobotString(Definition.ROBOT_SETTINGS_BATTERY_INFO)+"%");
 
-
+        tvInfo = (TextView) view.findViewById(R.id.robot_system_version);
+        tvInfo.setText("La version del sistema es: " + RobotApi.getInstance().getVersion());
+    }
 }
